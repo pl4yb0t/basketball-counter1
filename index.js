dd7d958cfd8a433eaba8b2e9524286b2fa4786ca
pl4yb0t <bert24@live.ca>
2022-11-09 14:26:49 -0500

    points counter

---
+++ index.js
@@ -0,0 +1,54 @@
+let scoreEl1 = document.getElementById("scoreEl1");
+let score1 = 0;
+
+let scoreEl2 = document.getElementById("scoreEl2");
+let score2 = 0;
+
+// Home Buttons
+function addOne1() {
+  score1 += 1;
+  scoreEl1.textContent = score1;
+}
+function addTwo1() {
+  score1 += 2;
+  scoreEl1.textContent = score1;
+}
+function addThree1() {
+  score1 += 3;
+  scoreEl1.textContent = score1;
+}
+
+//Guest Buttons
+
+function addOne2() {
+  score2 += 1;
+  scoreEl2.textContent = score2;
+}
+function addTwo2() {
+  score2 += 2;
+  scoreEl2.textContent = score2;
+}
+function addThree2() {
+  score2 += 3;
+  scoreEl2.textContent = score2;
+}
+
+//New Game Btn
+let message = document.getElementById("message");
+function newGame(){
+  if (score1 > score2) {
+    message.textContent="Home Team WON!!"
+
+  } else if (score2 > score1) {
+   message.textContent="Guest Team WON!!"
+
+ } else {
+   message.textContent="A Draw !! What a match !!"
+
+ }
+ score1 = 0;
+ score2 = 0;
+ scoreEl1.textContent = score1;
+ scoreEl2.textContent = score2;
+
+}
